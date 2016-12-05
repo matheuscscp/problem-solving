e8b68a0f439e0cec3658d1ffdb2d62cd1a42c31d
Matheus Pimenta <matheuscscp@gmail.com>
2016-12-05 17:56:23 -0200

    Uploading template

---
+++ lib/template.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// BEGIN template
+#define ff first
+#define ss second
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define eb emplace_back
+
+#define FASTIO  std::ios::sync_with_stdio(false)
+#define dbg(X)  cout << ">>> (" << #X << ") = (" << X << ")\n"
+#define _       << " _ " <<
+#define  rp(ITER,BEG,END) for (int ITER = BEG, ENDD = END; ITER <= ENDD; ITER++)
+#define rpd(ITER,BEG,END) for (int ITER = BEG, ENDD = END; ITER >= ENDD; ITER--)
+#define DEG2RAD(X)  (pi*(X)/180.0)
+
+enum {WHITE=0,GRAY,BLACK};
+enum {UNVISITED=-1};
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef complex<double> cplx;
+
+const double pi = 2*acos(0);
+const double tau = 2.0*pi;
+
+#define oo        0x3f3f3f3f3f3f3f3fL
+#define MAXLOGN   25
+#define MAX_PRIME 50000
+#define MOD       1000000007
+#define MAXN      200005
+// END template
+
+int main() {
+  return 0;
+}
