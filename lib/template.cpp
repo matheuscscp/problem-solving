872716d7bff59d02f5f64aa54f898944c73fb72f
Matheus Pimenta <matheuscscp@gmail.com>
2016-12-11 18:42:08 -0200

    Update template

--- lib/template.cpp
+++ lib/template.cpp
@@ -9,9 +9,14 @@ using namespace std;
 #define pb push_back
 #define eb emplace_back
 
-#define FASTIO  std::ios::sync_with_stdio(false)
-#define dbg(X)  cout << ">>> (" << #X << ") = (" << X << ")\n"
-#define _       << " _ " <<
+#define FASTIO      std::ios::sync_with_stdio(false)
+#define sci(X)      scanf("%d",&(X))
+#define sci2(X,Y)   scanf("%d%d",&(X),&(Y))
+#define sci3(X,Y,Z) scanf("%d%d%d",&(X),&(Y),&(Z))
+#define scl(X)      scanf("%lld",&(X))
+#define scs(X)      scanf("%s",X)
+#define dbg(X)      cerr << ">>> (" << #X << ") = (" << X << ")\n"
+#define _           << " _ " <<
 #define  rp(ITER,BEG,END) for (int ITER = BEG, ENDD = END; ITER <= ENDD; ITER++)
 #define rpd(ITER,BEG,END) for (int ITER = BEG, ENDD = END; ITER >= ENDD; ITER--)
 #define DEG2RAD(X)  (pi*(X)/180.0)
@@ -32,7 +37,7 @@ const double tau = 2.0*pi;
 #define MAXLOGN   25
 #define MAX_PRIME 50000
 #define MOD       1000000007
-#define MAXN      200005
+#define MAXN      1000005
 // END template
 
 int main() {
