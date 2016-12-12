aa271698c34048d5b838b1d4230fdf53b25d507f
Matheus Pimenta <matheuscscp@gmail.com>
2016-12-11 22:42:27 -0200

    Update template

--- lib/template.cpp
+++ lib/template.cpp
@@ -9,17 +9,24 @@ using namespace std;
 #define pb push_back
 #define eb emplace_back
 
-#define FASTIO      std::ios::sync_with_stdio(false)
+#ifdef ONLINE_JUDGE
+  #define FASTIO    std::ios::sync_with_stdio(false)
+  #define dbg(X)
+  #define _         _
+#else
+  #define FASTIO
+  #define dbg(X)    cerr << ">>> (" << #X << ") = (" << X << ")\n"
+  #define _         << " _ " <<
+#endif
 #define sci(X)      scanf("%d",&(X))
 #define sci2(X,Y)   scanf("%d%d",&(X),&(Y))
 #define sci3(X,Y,Z) scanf("%d%d%d",&(X),&(Y),&(Z))
 #define scl(X)      scanf("%lld",&(X))
 #define scs(X)      scanf("%s",X)
-#define dbg(X)      cerr << ">>> (" << #X << ") = (" << X << ")\n"
-#define _           << " _ " <<
 #define  rp(ITER,BEG,END) for (int ITER = BEG, ENDD = END; ITER <= ENDD; ITER++)
 #define rpd(ITER,BEG,END) for (int ITER = BEG, ENDD = END; ITER >= ENDD; ITER--)
 #define DEG2RAD(X)  (pi*(X)/180.0)
+#define LG(X)       (63 - __builtin_clzll(X))
 
 enum {WHITE=0,GRAY,BLACK};
 enum {UNVISITED=-1};
@@ -37,7 +44,7 @@ const double tau = 2.0*pi;
 #define MAXLOGN   25
 #define MAX_PRIME 50000
 #define MOD       1000000007
-#define MAXN      1000005
+#define MAXN      200005
 // END template
 
 int main() {
