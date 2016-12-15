64e16ed31ca1c7aaffaffcaa2cf49777c66fb310
Matheus Pimenta <matheuscscp@gmail.com>
2016-12-15 19:32:49 -0200

    Update template

--- lib/template.cpp
+++ lib/template.cpp
@@ -26,26 +26,17 @@ using namespace std;
 #define scs(X)      scanf("%s",X)
 #define  rp(ITER,BEG,END) for (int ITER = BEG, ENDD = END; ITER <= ENDD; ITER++)
 #define rpd(ITER,BEG,END) for (int ITER = BEG, ENDD = END; ITER >= ENDD; ITER--)
-#define DEG2RAD(X)  (pi*(X)/180.0)
 #define LG(X)       (63 - __builtin_clzll(X))
-
-enum {WHITE=0,GRAY,BLACK};
-enum {UNVISITED=-1};
 
 typedef long long ll;
 typedef pair<int,int> ii;
 typedef vector<int> vi;
 typedef vector<bool> vb;
-typedef complex<double> cplx;
-
-const double pi = 2*acos(0);
-const double tau = 2.0*pi;
 
 #define oo        0x3f3f3f3f3f3f3f3fLL
 #define MAXLOGN   25
-#define MAX_PRIME 50000
 #define MOD       1000000007
-#define MAXN      200005
+#define MAXN      2000005
 // END template
 
 int main() {
