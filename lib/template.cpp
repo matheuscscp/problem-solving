4981a5141b082d9645f83974db1d616720e07fa8
Matheus Pimenta <matheuscscp@gmail.com>
2016-12-21 19:36:50 -0200

    Update template

--- lib/template.cpp
+++ lib/template.cpp
@@ -2,42 +2,42 @@
 using namespace std;
 
 // BEGIN template
-#define ff first
-#define ss second
-#define mp make_pair
-#define mt make_tuple
-#define pb push_back
-#define eb emplace_back
-#define pq priority_queue
+#define   ff            first
+#define   ss            second
+#define   mp            make_pair
+#define   mt            make_tuple
+#define   pb            push_back
+#define   eb            emplace_back
+#define   pq            priority_queue
 
 #ifdef ONLINE_JUDGE
-  #define fastio    std::ios::sync_with_stdio(false)
+  #define fastio        std::ios::sync_with_stdio(false)
   #define dbg(X)
-  #define _         _
+  #define _             _
 #else
   #define fastio
-  #define dbg(X)    cerr << ">>> (" << #X << ") = (" << X << ")\n"
-  #define _         << " _ " <<
+  #define dbg(X)        cerr << ">>> (" << #X << ") = (" << X << ")\n"
+  #define _             << " _ " <<
 #endif
-#define sci(X)      scanf("%d",&(X))
-#define sci2(X,Y)   scanf("%d%d",&(X),&(Y))
-#define sci3(X,Y,Z) scanf("%d%d%d",&(X),&(Y),&(Z))
-#define scl(X)      scanf("%lld",&(X))
-#define scs(X)      scanf("%s",X)
-#define flush       fflush(stdout)
-#define lg(X)       (63 - __builtin_clzll(X))
-#define  rp(i,L,R)  for (int i = L, __R = R; i <= __R; i++)
-#define rpd(i,R,L)  for (int i = R, __L = L; __L <= i; i--)
+#define   sci(X)        scanf("%d",&(X))
+#define   sci2(X,Y)     scanf("%d%d",&(X),&(Y))
+#define   sci3(X,Y,Z)   scanf("%d%d%d",&(X),&(Y),&(Z))
+#define   scl(X)        scanf("%lld",&(X))
+#define   scs(X)        scanf("%s",X)
+#define   flush         fflush(stdout)
+#define   lg(X)         (63 - __builtin_clzll(X))
+#define    rp(i,L,R)    for (int i = L, __R = R; i <= __R; i++)
+#define   rpd(i,R,L)    for (int i = R, __L = L; __L <= i; i--)
 
-typedef long long ll;
-typedef pair<int,int> ii;
-typedef vector<int> vi;
-typedef vector<bool> vb;
+typedef   long long     ll;
+typedef   pair<int,int> ii;
+typedef   vector<int>   vi;
+typedef   vector<bool>  vb;
 
-#define oo        0x3f3f3f3f3f3f3f3fLL
-#define MAXLOGN   25
-#define MOD       1000000007
-#define MAXN      100005
+#define   oo            0x3f3f3f3f3f3f3f3fLL
+#define   MAXLOGN       25
+#define   MOD           1000000007
+#define   MAXN          200005
 // END template
 
 int main() {
