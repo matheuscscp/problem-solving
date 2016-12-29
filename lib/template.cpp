2818f57f435a5a4881ca47ae0f7bb89438a4b0e6
Matheus Pimenta <matheuscscp@gmail.com>
2016-12-29 03:00:18 -0200

    Update template

--- lib/template.cpp
+++ lib/template.cpp
@@ -30,6 +30,30 @@ using namespace std;
 #define    rp(i,L,R)    for (int i = L, __R = R; i <= __R; i++)
 #define   rpd(i,R,L)    for (int i = R, __L = L; __L <= i; i--)
 
+#define DP1(type,X)\
+  static type dp[X];\
+  static bool mark[X];\
+  auto& ans = dp[i];\
+  if (mark[i]) return ans;\
+  mark[i] = true;
+
+#define DP2(type,X,Y)\
+  static type dp[X][Y];\
+  static bool mark[X][Y];\
+  auto& ans = dp[i][j];\
+  if (mark[i][j]) return ans;\
+  mark[i][j] = true;
+
+#define DP3(type,X,Y,Z)\
+  static type dp[X][Y][Z];\
+  static bool mark[X][Y][Z];\
+  auto& ans = dp[i][j][k];\
+  if (mark[i][j][k]) return ans;\
+  mark[i][j][k] = true;
+
+#define GET_DP(_1,_2,_3,_4,NAME,...) NAME
+#define DP(...) GET_DP(__VA_ARGS__,DP3,DP2,DP1)(__VA_ARGS__)
+
 typedef   long long     ll;
 typedef   pair<int,int> ii;
 typedef   vector<int>   vi;
@@ -38,7 +62,7 @@ typedef   vector<bool>  vb;
 #define   oo            0x3f3f3f3f3f3f3f3fLL
 #define   MAXLOGN       25
 #define   MOD           int(1e9+7)
-#define   MAXN          int(1e5+5)
+#define   MAXN          int(1e6+5)
 // END template
 
 int main() {
