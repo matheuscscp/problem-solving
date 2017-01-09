b05a03eae6abe7a115a53318c629294f4fca2d2b
Matheus Pimenta <matheuscscp@gmail.com>
2017-01-09 05:14:19 -0200

    Uploading python3 template

---
+++ lib/template.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+# BEGIN template
+import sys
+import re
+import pprint
+
+def dbg(x,y=''):
+  if len(y) > 0: y += ' = '
+  sys.stderr.write('\n>>> '+y+pprint.pformat(x)+'\n')
+
+oo = 0x3f3f3f3f3f3f3f3f
+# END template
+
+def main():
+  
+
+main()
