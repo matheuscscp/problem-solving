745f820fc147e7a46dcc5ae0bc88d848a7c62a5c
Matheus Pimenta <matheuscscp@gmail.com>
2017-01-31 12:42:06 -0200

    Update parallel bfs

--- lib/graphs/parallel_bfs.cpp
+++ lib/graphs/parallel_bfs.cpp
@@ -68,88 +68,102 @@ typedef   vector<bool>  vb;
 
 #define   oo            0x3f3f3f3f3f3f3f3fLL
 #define   LGN           25
-#define   MOD           int(1e9+7)
-#define   N             int(2e5+5)
+#define   MOD           ll(99990001)
+#define   N             int(4e5+5)
 // END template
 
-// tree implementation
-int to[N/*ed*/]/*vx*/, nxtedge = 1/*ed*/;
-int head[N/*vx*/]/*ed*/, nxt[N/*ed*/]/*ed*/;
-bool off[N/*ed*/];
-map<ii,int> idx/*ed*/;
-int color[N/*vx*/], nxtcolor = 1, cnt[N/*cl*/];
-void add(int u, int v) {
-  int e = nxtedge++;
+// http://codeforces.com/group/eqgxxTNwgd/contest/100109/problem/K
+
+int n, x[N], y[N]; ll w[N];
+
+int nxedge = 2, nx[N], hd[N], to[N], mark[N], pass = 1; bool off[N];
+ll color[N], nxcolor = 1, lazy[N];
+int frwd(int i) { return 2*i+2; }
+int bkwd(int i) { return 2*i+3; }
+int idx(int e) { return (e-2)/2; }
+
+void add_edge(int u, int v) {
+  int e = nxedge++;
+  nx[e] = hd[u];
+  hd[u] = e;
   to[e] = v;
-  nxt[e] = head[u];
-  head[u] = e;
-  idx[ii(u,v)] = e;
 }
-void remove(int u, int v) {
-  off[idx[ii(u,v)]] = true;
+void remove_edge(int e) {
+  off[e] = true;
 }
-int edge(int e) { // meta-path compression
+int edge(int e) {
   if (!e || !off[e]) return e;
-  return nxt[e] = edge(nxt[e]);
+  return nx[e] = edge(nx[e]);
 }
-void paint(int u, int c, int p = 0) {
-  color[u] = c, cnt[c]++;
-  for (int e = edge(head[u]); e; e = edge(nxt[e])) if (to[e] != p) {
-    paint(to[e],c,u);
+ll prop(int e) {
+  return w[idx(e)] = (w[idx(e)]+lazy[color[to[e]]])%MOD;
+}
+void paint(int u, int c, ll z) {
+  color[u] = c;
+  for (int e = edge(hd[u]); e; e = edge(nx[e])) if (color[to[e]] != c) {
+    int v = to[e];
+    w[idx(e)] = (prop(e)*z)%MOD;
+    paint(v,c,z);
   }
 }
-bool connected(int u, int v) { return color[u] == color[v]; }
 
-// http://codeforces.com/group/eqgxxTNwgd/contest/100960/problem/H
 int main() {
   fastio;
-  int n;
+  // input tree
   sci(n);
-  rp(u,1,n) paint(u,nxtcolor++);
-  char cmd[10];
-  while (true) {
-    scs(cmd);
-    if (cmd[0] == 'E') return 0;
-    int a,b;
-    sci(a,b);
-    switch (cmd[0]) {
-      case 'C': {
-        if (cnt[color[a]] > cnt[color[b]]) swap(a,b);
-        paint(a,color[b]);
-        add(a,b);
-        add(b,a);
-        break;
-      }
-      case 'D': {
-        remove(a,b);
-        remove(b,a);
-        queue<ii> q[2];
-        q[0].push(ii(a,0)), q[1].push(ii(b,0));
-        int e[2] = {edge(head[a]),edge(head[b])};
-        int i = 0;
-        while (!q[i].empty()) {
-          if (!e[i]) {
-            q[i].pop();
-            if (!q[i].empty()) e[i] = edge(head[q[i].front().ff]);
-            continue;
-          }
-          if (to[e[i]] == q[i].front().ss) {
-            e[i] = edge(nxt[e[i]]);
-            continue;
-          }
-          q[i].push(ii(to[e[i]],q[i].front().ff));
-          e[i] = edge(nxt[e[i]]);
-          i ^= 1;
-        }
-        if (i == 1) swap(a,b);
-        paint(a,nxtcolor++);
-        break;
-      }
-      case 'T':
-        printf(connected(a,b) ? "YES\n" : "NO\n");
-        flush;
-        break;
+  rp(i,0,n-2) {
+    sci(x[i],y[i]);
+    scl(w[i]);
+    w[i] %= MOD;
+    add_edge(x[i],y[i]);
+    add_edge(y[i],x[i]);
+  }
+  // queries
+  rp(j,2,n) {
+    // input
+    int i;
+    sci(i);
+    i--;
+    // answer
+    ll z = prop(frwd(i));
+    // output
+    printf("%lld\n",z);
+    flush;
+    // remove edges
+    remove_edge(frwd(i));
+    remove_edge(bkwd(i));
+    // parallel bfs
+    pass++;
+    int a = x[i], b = y[i];
+    queue<int> q[2];
+    q[0].push(edge(hd[a])), q[1].push(edge(hd[b]));
+    mark[a] = pass, mark[b] = pass;
+    int mv[] = {a,b};
+    int t = 0;
+    while (!q[t].empty()) {
+      int e = q[t].front(); q[t].pop();
+      if (!e) continue;
+      q[t].push(edge(nx[e]));
+      if (mark[to[e]] == pass) continue;
+      mark[to[e]] = pass;
+      q[t].push(edge(hd[to[e]]));
+      mv[t] = min(mv[t],to[e]);
+      t ^= 1;
     }
+    bool ok = true;
+    while (ok && !q[!t].empty()) {
+      int e = q[!t].front(); q[!t].pop();
+      if (!e) continue;
+      q[!t].push(edge(nx[e]));
+      if (mark[to[e]] == pass) continue;
+      ok = false;
+    }
+    // get smaller component
+    if (ok && !t) if (mv[0] > mv[1]) t = 1;
+    if (t) swap(a,b);
+    // paint and lazy
+    paint(a,nxcolor++,z);
+    lazy[color[b]] = (lazy[color[b]]+z)%MOD;
   }
   return 0;
 }
