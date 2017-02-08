a6935503d205a0c643d57dbacbceb2d6c03059e9
Matheus Pimenta <matheuscscp@gmail.com>
2017-02-08 10:52:34 -0200

    Uploading digit dp

--- lib/dynamic_programming/digit_dp.cpp
+++ lib/dynamic_programming/digit_dp.cpp
@@ -31,8 +31,8 @@ using namespace std;
 #define   flush         fflush(stdout)
 #define   lg(X)         (63 - __builtin_clzll(X))
 #define   LG(X)         (lg(X)+((1<<lg(X)) < (X)))
-#define    rp(i,L,R)    for (ll i = L, __R = R; i <= __R; i++)
-#define   rpd(i,R,L)    for (ll i = R, __L = L; __L <= i; i--)
+#define    rp(i,L,R)    for (int i = L, __R = R; i <= __R; i++)
+#define   rpd(i,R,L)    for (int i = R, __L = L; __L <= i; i--)
 
 #define GET_SCI(_1,_2,_3,NAME,...) NAME
 #define sci(...) GET_SCI(__VA_ARGS__,sci3,sci2,sci1)(__VA_ARGS__)
@@ -68,10 +68,64 @@ typedef   vector<bool>  vb;
 
 #define   oo            0x3f3f3f3f3f3f3f3fLL
 #define   LGN           25
-#define   MOD           int(1e9+7)
+#define   MOD           int(1e9+9)
 #define   N             int(1e3+5)
 // END template
 
+// http://codeforces.com/group/eqgxxTNwgd/contest/100345/problem/D
+
+#define   get(d,m)      (((m)>>((d)<<1))&3)
+#define   sub(d,m)      (((m)&~(3<<((d)<<1)))|((get(d,m)-1)<<((d)<<1)))
+
+const int full = 0x0aaaaa;
+
+// number of i-length strings in j state satisfying the properties
+ll f(int i, int j) {
+  if (i == 0) return 1;
+  DP(ll,19,1<<20);
+  rp(d,0,9) if (get(d,j)) ans += f(i-1,sub(d,j));
+  return ans;
+}
+
+ll g(ll x) {
+  if (x == 0) return 0;
+  // get string and length
+  stringstream ss;
+  ss << x;
+  string s;
+  ss >> s;
+  int len = s.size();
+  // start answer computation
+  ll ans = 0;
+  // shorter than x
+  rp(l,1,len-1) rp(d,1,9) ans += f(l-1,sub(d,full));
+  // same length as x
+    // first digit
+    rp(d,'1',s[0]-1) ans += f(len-1,sub(d-'0',full));
+    int state = sub(s[0]-'0',full);
+    // other digits
+    bool nomore = false;
+    rp(i,1,len-1) {
+      rp(d,'0',s[i]-1) if (get(d-'0',state)) ans += f(len-1-i,sub(d-'0',state));
+      if (!get(s[i]-'0',state)) {
+        nomore = true;
+        break;
+      }
+      state = sub(s[i]-'0',state);
+    }
+    // number itself
+    if (!nomore) ans++;
+  return ans;
+}
+
 int main() {
+#ifdef ONLINE_JUDGE
+  freopen("exchange.in","r",stdin);
+  freopen("exchange.out","w",stdout);
+#endif
+  fastio;
+  ll l,r;
+  scl(l), scl(r);
+  printf("%lld\n",g(r)-g(l-1));
   return 0;
 }
