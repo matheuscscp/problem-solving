d81205385419361890b06d947c05ca54f5e0ee54
Matheus Pimenta <matheuscscp@gmail.com>
2017-02-11 19:37:42 -0200

    Uploading prefix function and kmp

---
+++ lib/strings/prefix_function_and_kmp.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> prefix_function(const string& s) {
+  int n = s.size();
+  vector<int> p(n+1,0);
+  for (int q = 1; q < n; q++) {
+    int u = q+1;
+    p[u] = p[q]; // start with parent failure
+    while (p[u] && s[p[u]] != s[q]) p[u] = p[p[u]]; // go through failure links
+    p[u] = p[u] ? p[u]+1 : (s[0]==s[q]); // final failure goes through the edge
+  }
+  return p;
+}
+
+void kmp(const string& p, const string& t, const function<void(int)>& outfunc) {
+  int n = p.size();
+  auto pf = prefix_function(p);
+  int q = 0, len = 0;
+  for (const char* s = t.c_str(); *s; s++) {
+    while (q && p[q] != *s) q = pf[q]; // go through failure links
+    q = q ? q+1 : (p[0]==*s); // final failure goes through the edge
+    len++;
+    if (q == n) outfunc(len-n);
+  }
+}
+
+int main() {
+  string t,p;
+  cin >> t >> p;
+  kmp(p,t,[](int pos) {
+    printf("occurrence at %d\n",pos);
+  });
+  return 0;
+}
